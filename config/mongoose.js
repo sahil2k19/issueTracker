d0adaf600d63cce5da96624a620673bb13c1957b
sahil2k19 <sahilgaga227@gmail.com>
2022-11-20 21:04:34 +0530

    change in mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,23 +6,25 @@ const mongoose = require('mongoose');
 let DB_URL = process.env.DB_URL;
 
 //connect to the database
-// mongoose.connect(DB_URL ,
-//   {
-//     usenewurlparser: true,
-//     useunifiedtopology: true
-//   }).then(()=>{
-//     console.log(`connection successful `);
-//   }).catch((err)=>{
-//     console.log(`error connecting to database` , err);
-//   })
+mongoose
+  .connect(DB_URL, {
+    usenewurlparser: true,
+    useunifiedtopology: true,
+  })
+  .then(() => {
+    console.log(`connection successful `);
+  })
+  .catch((err) => {
+    console.log(`error connecting to database`, err);
+  });
 
 // acquire connection (to check if its successful)
-const db = mongoose.connection;
-mongoose.connect('mongodb://localhost/myDB');
-db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+// const db = mongoose.connection;
+// mongoose.connect('mongodb://localhost/myDB');
+// db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
 
-db.once('open', function () {
-  console.log('Connected to Database :: MongoDB');
-});
+// db.once('open', function () {
+//   console.log('Connected to Database :: MongoDB');
+// });
 
 module.exports = db;
